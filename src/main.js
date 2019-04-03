ac0b360d8380eee08b2d0efd0b6e06bc69d2f6fa
konstantin.shuvalov <konstantin.shuvalov@nordigy.ru>
2019-04-03 12:27:05 +0300

    reintend JS from Tab\4 Space to 2 Space standard

--- src/main.js
+++ src/main.js
@@ -6,32 +6,32 @@ Vue.config.productionTip = false
 window.app = new Vue({render: h => h(App),}).$mount('#app');
 
 function connected(p) {
-	p.onMessage.addListener(function(m) {
-		new_data({
-			type: m.type,
-			data: m.message,
-			length: m.message.length,
-			time: new Date()
-		});
+  p.onMessage.addListener(function(m) {
+    new_data({
+      type: m.type,
+      data: m.message,
+      length: m.message.length,
+      time: new Date()
     });
+  });
 }
 
 browser.runtime.onConnect.addListener(connected);
 
 function new_data(data) {
-	app.$children[0].ws_data.push(data);
+  app.$children[0].ws_data.push(data);
 }
 
 function handleError(error) {
-	if (error.isError) {
-		console.log(`Devtools error: ${error.code}`);
-	} else {
-		console.log(`JavaScript error: ${error.value}`);
-	}
+  if (error.isError) {
+    console.log(`Devtools error: ${error.code}`);
+  } else {
+    console.log(`JavaScript error: ${error.value}`);
+  }
 }
 
 function handleResult(result) {
-	if (result[1]) {
-		handleError(result[1]);
-	}
+  if (result[1]) {
+    handleError(result[1]);
+  }
 }
